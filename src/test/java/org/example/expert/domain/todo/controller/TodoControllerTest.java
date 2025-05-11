a650cc525b1257e4db3ec049f5f582ca393703aa
sun <hsunvirus@gmail.com>
2025-05-11 11:49:33 +0900

    refactor: fix level1

--- src/test/java/org/example/expert/domain/todo/controller/TodoControllerTest.java
+++ src/test/java/org/example/expert/domain/todo/controller/TodoControllerTest.java
@@ -1,34 +1,85 @@
 package org.example.expert.domain.todo.controller;
 
+import org.example.expert.config.AuthUserArgumentResolver;
+import org.example.expert.domain.common.annotation.Auth;
 import org.example.expert.domain.common.dto.AuthUser;
 import org.example.expert.domain.common.exception.InvalidRequestException;
+import org.example.expert.domain.todo.dto.request.TodoSaveRequest;
 import org.example.expert.domain.todo.dto.response.TodoResponse;
+import org.example.expert.domain.todo.dto.response.TodoSaveResponse;
 import org.example.expert.domain.todo.service.TodoService;
 import org.example.expert.domain.user.dto.response.UserResponse;
 import org.example.expert.domain.user.entity.User;
 import org.example.expert.domain.user.enums.UserRole;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.core.MethodParameter;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
 
 import java.time.LocalDateTime;
 
+import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@WebMvcTest(TodoController.class)
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+// @WebMvcTest(TodoController.class)
+@ExtendWith(MockitoExtension.class)
 class TodoControllerTest {
 
-    @Autowired
     private MockMvc mockMvc;
 
-    @MockBean
+    @Mock
     private TodoService todoService;
+
+    @InjectMocks
+    private TodoController todoController;
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    @BeforeEach
+    void setUp() {
+        mockMvc = MockMvcBuilders.standaloneSetup(todoController)
+            .setCustomArgumentResolvers(new TestAuthUserArgumentResolver())
+            .build();
+    }
+
+    @Test
+    @DisplayName("todo 저장 성공 테스트")
+    void todo_저장_성공() throws Exception {
+        // given
+        AuthUser authUser = new AuthUser(1L, "test@example.com",UserRole.USER);
+        User user = User.fromAuthUser(authUser);
+
+        TodoSaveRequest request = new TodoSaveRequest("title","content");
+        TodoSaveResponse response = new TodoSaveResponse(1L,"title","content","Sunny",new UserResponse(user.getId(),user.getEmail()));
+
+        // when
+        when(todoService.saveTodo(any(AuthUser.class),any(TodoSaveRequest.class))).thenReturn(response);
+
+        // then
+        mockMvc.perform(post("/todos")
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(objectMapper.writeValueAsString(request)))
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$.id").value(1L))
+            .andExpect(jsonPath("$.title").value("title"));;
+    }
 
     @Test
     void todo_단건_조회에_성공한다() throws Exception {
@@ -73,5 +124,20 @@ class TodoControllerTest {
                 .andExpect(jsonPath("$.status").value(HttpStatus.OK.name()))
                 .andExpect(jsonPath("$.code").value(HttpStatus.OK.value()))
                 .andExpect(jsonPath("$.message").value("Todo not found"));
+    }
+
+    public class TestAuthUserArgumentResolver implements HandlerMethodArgumentResolver {
+        @Override
+        public boolean supportsParameter(MethodParameter parameter) {
+            return parameter.hasParameterAnnotation(Auth.class);  // 너희 @Auth 조건
+        }
+
+        @Override
+        public Object resolveArgument(MethodParameter parameter,
+            ModelAndViewContainer mavContainer,
+            NativeWebRequest webRequest,
+            WebDataBinderFactory binderFactory) {
+            return new AuthUser(1L, "test@example.com", UserRole.USER); // Dummy 유저
+        }
     }
 }
---
+++ src/test/java/org/example/expert/domain/todo/service/TodoServiceTest.java
@@ -0,0 +1,65 @@
+package org.example.expert.domain.todo.service;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.BDDMockito.*;
+
+import org.example.expert.client.WeatherClient;
+import org.example.expert.domain.common.dto.AuthUser;
+import org.example.expert.domain.todo.dto.request.TodoSaveRequest;
+import org.example.expert.domain.todo.dto.response.TodoSaveResponse;
+import org.example.expert.domain.user.entity.User;
+import org.example.expert.domain.user.enums.UserRole;
+import org.example.expert.domain.user.repository.UserRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+@Transactional
+@ActiveProfiles("test")
+class TodoServiceTest {
+
+	@Autowired
+	private TodoService todoService;
+
+	@MockBean
+	private WeatherClient weatherClient;
+
+	@Autowired
+	private UserRepository userRepository;
+
+	@Test
+	@DisplayName("Todo 저장 시 실패")
+	void saveTodo() {
+		AuthUser authUser = new AuthUser(1L, "test@example.com", UserRole.USER);
+		User user = User.fromAuthUser(authUser);
+		userRepository.save(user);
+
+		TodoSaveRequest request = new TodoSaveRequest("test title", "test contents");
+
+		// WeatherClient는 Mock -> 항상 "Sunny" 리턴
+		given(weatherClient.getTodayWeather()).willReturn("Sunny");
+
+		TodoSaveResponse response = todoService.saveTodo(authUser, request);
+
+		assertThat(response).isNotNull();
+		assertThat(response.getId()).isNotNull(); // DB에 저장되어야 id가 있음
+		assertThat(response.getTitle()).isEqualTo("test title");
+		assertThat(response.getWeather()).isEqualTo("Sunny");
+		assertThat(response.getUser().getEmail()).isEqualTo("test@example.com");
+	}
+
+	@Test
+	void getTodos() {
+	}
+
+	@Test
+	void getTodo() {
+	}
+}
